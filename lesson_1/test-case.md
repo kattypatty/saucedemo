f8aa6af6c83641925a4b9ab8fe149127ad2214f0
kattypatty <kat.patigorez@gmail.com>
2024-03-26 15:50:13 -0700

    added test_placing_order automation code

--- lesson_1/test-case.md
+++ lesson_1/test-case.md
@@ -12,11 +12,13 @@
 2. Removing an item from the cart via the cart
 3. Adding a product to the cart from the product card
 4. Removing an item from the cart via the item page
-
+-------------------------------------------------------------------------------
 # Item page
 
 1. Successful transition to the item page after clicking on the product image
 2. Successful transition to the item page after clicking on the product name
+-------------------------------------------------------------------------------
+# Placing an order
 
-
+1. Placing an order using correct data
 
--- lesson_1/test_auth.py
+++ lesson_1/test_auth.py
@@ -34,17 +34,48 @@ def test_shopping_cart():
 def test_item_page():
     test_auth_positive()
     assert browser.current_url == 'https://www.saucedemo.com/v1/inventory.html'
+
+    # On invenroty page, click on item image and page successfully transfers
     browser.find_elements(By.XPATH, "//*[@id='item_4_img_link']/img")[0].click()
     assert browser.current_url == 'https://www.saucedemo.com/v1/inventory-item.html?id=4', browser.current_url
     
+    # On image page, click on "back" button to come back to the inventory page
     browser.find_element(By.XPATH, "//*[@class='inventory_details_back_button']").click()
-    
+
+    #  On invenroty page, click on a product name and page successfully transfers 
     browser.find_element(By.XPATH, "//*[@id='item_4_title_link']/div").click()
     assert browser.current_url == 'https://www.saucedemo.com/v1/inventory-item.html?id=4', browser.current_url
-    time.sleep(100)
+    
+
+def test_placing_order():
+    test_auth_positive()
+
+    # On inventory page, click on "Add to cart" button.
+    browser.find_element(By.XPATH, "//*[@class='btn_primary btn_inventory']").click()
+
+    # On inventory page, click on the cart.
+    browser.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a").click()
+
+    # On an item page, click on "checkout" button and page successfully transfers 
+    browser.find_element(By.XPATH, "//*[@class='btn_action checkout_button']").click()
+    assert browser.current_url == 'https://www.saucedemo.com/v1/checkout-step-one.html', browser.current_url
+    
+    # Placing customer details in the checkout info
+    browser.find_element('xpath', "//*[@id='first-name']").send_keys('Vasya')
+    browser.find_element('xpath', "//*[@id='last-name']").send_keys('Petrov')
+    browser.find_element('xpath', "//*[@id='postal-code']").send_keys('98660')
+
+    time.sleep(5)
+    # On checkout information page, click on "continue" button and page successfully transfers
+    browser.find_element(By.XPATH, "//*[@class='btn_primary cart_button']").click()
+    time.sleep(5)
+    assert browser.current_url == 'https://www.saucedemo.com/v1/checkout-step-two.html', browser.current_url
+
 
     # browser.quit()
 
 
+
 test_shopping_cart()
 test_item_page()
+test_placing_order()
