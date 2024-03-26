bfaf3ab42d42a107cdd4068326db02117b0cabce
kattypatty <kat.patigorez@gmail.com>
2024-03-25 20:48:36 -0700

    add authorization using correct data

--- lesson_1/test_auth.py
+++ lesson_1/test_auth.py
@@ -1,3 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-driver=webdriver.Chrome
+browser=webdriver.Chrome()
+
+def test_auth_positive():
+    browser.get('https://www.saucedemo.com/v1/')
+
+    browser.find_element('xpath', '//*[@id="user-name"]').send_keys('standard_user')
+    browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
+    browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    assert browser.current_url == 'https://www.saucedemo.com/v1/inventory.html', 'url не соответствует ожидаемому'
+
+def test_shopping_cart():
+    test_auth_positive()
+    browser.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
+
+    time.sleep(10)
+
+    # browser.quit()
+
+
+test_shopping_cart()
